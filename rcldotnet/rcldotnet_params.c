7ed27b55a0a4b3ce9b117f3cfd44957b89e2c35a
Stefan Hoffmann <stefan.hoffmann@schiller.de>
2025-01-10 10:50:49 +0100

    use rosidl_runtime_c__String__assign instead of our own function

--- rcldotnet/rcldotnet_params.c
+++ rcldotnet/rcldotnet_params.c
@@ -16,6 +16,7 @@
 #include <rcl_yaml_param_parser/parser.h>
 
 #include <rosidl_runtime_c/primitives_sequence.h>
+#include <rosidl_runtime_c/string_functions.h>
 
 #include <rcl_interfaces/msg/parameter_value.h>
 #include <rcl_interfaces/msg/parameter_type.h>
@@ -33,23 +34,6 @@ ROSIDL_RUNTIME_C__PRIMITIVE_SEQUENCE(void, void)
 
 void native_rcl_destroy_rcl_params(void *rcl_params) {
   rcl_yaml_node_struct_fini((rcl_params_t *)rcl_params);
-}
-
-void rcldotnet_params_copy_to_rosidl_runtime_c__String(rosidl_runtime_c__String *dest, const char * src) {
-  size_t length = strlen(src);
-  size_t capacity = sizeof(char) * (length + 1);
-  dest->size = length;
-
-  if (dest->capacity != capacity) {
-    if (dest->capacity != 0) {
-      free(dest->data);
-    }
-
-    dest->capacity = capacity;
-    dest->data = malloc(capacity);
-  }
-
-  memcpy(dest->data, src, capacity);
 }
 
 void rcldotnet_params_copy_yaml_array_to_parameter_array(rosidl_runtime_c__void__Sequence *dest, const rcl_void_array_t *src, size_t element_size) {
@@ -87,6 +71,7 @@ void rcldotnet_params_copy_yaml_string_array_to_parameter_string_array(rosidl_ru
     
     // If the elements aren't initialised, free may be run on an uninitialised data pointer.
     for (int i = 0; i < length; i++) {
+      dest->data[i].data = NULL;
       dest->data[i].capacity = 0;
       dest->data[i].size = 0;
     }
@@ -95,8 +80,7 @@ void rcldotnet_params_copy_yaml_string_array_to_parameter_string_array(rosidl_ru
   dest->size = length;
 
   for (int i = 0; i < length; i++) {
-    rosidl_runtime_c__String *dest_element = &dest->data[i];
-    rcldotnet_params_copy_to_rosidl_runtime_c__String(&(dest->data[i]), src->data[i]);
+    rosidl_runtime_c__String__assign(&(dest->data[i]), src->data[i]);
   }
 }
 
@@ -132,7 +116,7 @@ bool rcldotnet_params_try_get_parameter_from_node_params(const rcl_node_params_t
     break;
     case rcl_interfaces__msg__ParameterType__PARAMETER_STRING:
       if (rcl_param_value->string_value == NULL) return true;
-      rcldotnet_params_copy_to_rosidl_runtime_c__String(&param_value->string_value, rcl_param_value->string_value);
+      rosidl_runtime_c__String__assign(&param_value->string_value, rcl_param_value->string_value);
     break;
     case rcl_interfaces__msg__ParameterType__PARAMETER_BYTE_ARRAY:
       // Byte array parameter loading from YAML not implemented in RCL.
